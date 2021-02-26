7398fa0ed7e93b101517460a26d87ee43f2dec5e
Errors-Everywhere <AhmedMagd@users.noreply.github.com>
2021-02-26 22:06:05 +0300

    KUKA I IWA
    Added Psuedoinverse and Null Space methods to KUKA IIWA

--- Magd Codes/Redundancy.m
+++ Magd Codes/Redundancy.m
@@ -4,14 +4,15 @@ clc
 
 %% Redundancy:
 
-l = [1 1 1 1];
-jointsRange = [pi/6 pi/2; -pi/2 pi/2; -pi/2 pi/2; -pi/2 pi/2];
-q = [50 0 1 1]'; % initial configuration
-final_config = [50 50 50 50]';
+l = [340 200 200 200 200 126 4];
+jointsRange = deg2rad([30 90; 0 360; 0 360; 0 360; 0 360; 0 360; 0 360]);
+
+q = [20 10 10 20 10 10 10]'; % initial configuration
+final_config = [90 50 40 20 80 70 20]';
 
 %% Pseudo Inverse Solution
-[all_configs, all_positions] = PseudoInverseIK(q, final_config, 100, l);
+% [all_configs, all_positions] = PseudoInverseIK(q, final_config, l);
 
 %% Null Space Solution:
-% objective = "range";
-% [all_configs, all_positions] = NullSpaceIK(q, final_config, 100, l, objective, jointsRange);
+objective = "range";
+[all_configs, all_positions] = NullSpaceIK(q, final_config, l, objective, jointsRange);
